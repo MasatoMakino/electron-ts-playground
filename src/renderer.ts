a49d704b2707c896c827c94471ef5e8809194d24
MasatoMakino <unagiinu.g@gmail.com>
2020-12-26 16:40:58 +0900

    reformat : ts and css

--- src/renderer.ts
+++ src/renderer.ts
@@ -26,6 +26,8 @@
  * ```
  */
 
-import '../src/index.css';
+import "../src/index.css";
 
-console.log('👋 This message is being logged by "renderer.js", included via webpack');
+console.log(
+  '👋 This message is being logged by "renderer.js", included via webpack'
+);
