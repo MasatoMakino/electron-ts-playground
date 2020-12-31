7fe1f586e8182f63f2b290c2a009c145697de6e3
MasatoMakino <unagiinu.g@gmail.com>
2020-12-31 12:48:06 +0900

    change : api object to api class.

--- src/preload/preload.ts
+++ src/preload/preload.ts
@@ -1,18 +1,25 @@
 import { IpcRendererEvent } from "electron";
-
 const { contextBridge, ipcRenderer } = require("electron");
-import {IpcChannelType} from "./IpcChannelType";
+import { IpcChannelType } from "./IpcChannelType";
 
-contextBridge.exposeInMainWorld("api", {
-  sendToMainHello: () => {
+export class ContextBridgeApi {
+  constructor() {}
+
+  public sendToMainHello = () => {
     return ipcRenderer
-      .invoke(IpcChannelType.HELLO_TO_MAIN, { message: "hello to main, from renderer" })
-      .then((result: any) => result)
-      .catch((e: any) => console.log(e));
-  },
-  sendToRendererHello: (listener: (arg0: any) => any) => {
-    ipcRenderer.on("hello-to-renderer", (event: IpcRendererEvent, arg: any) =>
-      listener(arg)
+      .invoke(IpcChannelType.HELLO_TO_MAIN, {
+        message: "hello to main, from renderer",
+      })
+      .then((result: string) => result)
+      .catch((e: Error) => console.log(e));
+  };
+
+  public sendToRendererHello = (listener: (arg0: any) => any) => {
+    ipcRenderer.on(
+      IpcChannelType.HELLO_TO_RENDERER,
+      (event: IpcRendererEvent, arg: any) => listener(arg)
     );
-  },
-});
+  };
+}
+
+contextBridge.exposeInMainWorld("api", new ContextBridgeApi());
